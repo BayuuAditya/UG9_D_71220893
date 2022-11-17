8f1fb3ad39530ec92f8eaaec8a74279e085b9fe7
BayuuAditya <117901006+BayuuAditya@users.noreply.github.com>
2022-11-17 16:24:33 +0700

    Add files via upload

---
+++ UG9_D_71220893/1_D_71220893.py
@@ -0,0 +1,11 @@
+print("Test Case 1")
+inputan = input("Masukan jari-jari:")
+bilangan_1=4
+print(22/7*bilangan_1*bilangan_1)
+
+print("Test case 2")
+inputan = input("Masukan jari-jari:")
+bilangan_2=15
+print(22/7*bilangan_2*bilangan_2)
+
+
---
+++ UG9_D_71220893/3_D_71220893.py
@@ -0,0 +1,7 @@
+inputan = input ("Masukan Nama Anda:")
+inputan = input ("Masukan mata kuliah:")
+inputan = input ("Masukan Grup :")
+pisah = inputan.split(" ")
+depan = pisah [0]
+belakang = pisah [1]
+print("Halo",belakang,",",depan)
